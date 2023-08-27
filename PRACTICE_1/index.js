8de64e8f68712360404f595018a4946396cc0e39
muneebchiko <muneebnadeem900@gmail.com>
2023-08-27 20:14:17 +0500

    First assignment

---
+++ PRACTICE_1/index.js
@@ -0,0 +1,39 @@
+console.log ("hello world");
+console.log("Pakistan zindabad");
+
+let message ='hello world';
+console.log('Touseef '+ message); 
+message2='ali is a gay';
+console.log(message);
+console.log(message); // alt + shift + down arrow
+console.log(message2); // alt + shift + down arrow
+
+//variable declration
+
+let number1= 20;
+console.log(number1);
+
+//constant declaration
+
+const num1= 200;
+console.log(num1 + number1)
+ 
+let a=20;
+let b=30;
+let c=a*b;
+let d=a/b;
+
+console.log(a+b);
+console.log(c+d);
+let string1= 'hello'; 
+
+console.log(string1 + ' ' + a)
+
+let name = 'zoya';
+let age ='20';
+//let statement = `my name is____________ and i Am _______ years old`;  for different datatype
+let statement = `my name is ${name} and i Am ${age} years old`;   // template literals
+console.log(statement)
+
+let state= `${string1} ${age}`;   // for spaces
+console.log(state)
